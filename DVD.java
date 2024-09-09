2403116b5b6590c0de49a8f42bf10452a901ca5d
AurelDwiBintari <aurel_bintari_33rpl@student.smktelkom-mlg.sch.id>
2024-09-09 12:02:42 +0700

    Add files via upload

---
+++ DVD.java
@@ -0,0 +1,48 @@
+//Subclass
+public class DVD extends Product{
+    int length;
+    String Rating;
+    String Studio;
+
+    public DVD() {
+        super();
+        length = 0;
+        Rating = "";
+        Studio = "";
+    }
+        //Constructorparameter
+    public DVD(int Number, String Nama, int Quantity, double Price, int length, String Rating, String Studio){
+        super(Number, Nama, Quantity, Price );
+        this.length = length;
+        this.Rating = Rating;
+        this.Studio = Studio;
+
+    }
+
+    
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getRating() {
+        return this.Rating;
+    }
+
+    public void setRating(String Rating) {
+        this.Rating = Rating;
+    }
+
+    public String getStudio() {
+        return this.Studio;
+    }
+
+    public void setStudio(String Studio) {
+        this.Studio = Studio;
+    }
+
+}
